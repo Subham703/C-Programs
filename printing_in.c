1810ef33190c47103db83eea23894ea886d8977b
Subham Chetry <305707492+Subham703@users.noreply.github.com>
2026-07-19 21:59:03 +0530

    addition

--- printing_in.c
+++ printing_in.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
-int main() {
-    printf("1,2,3,4,5,6,7");
+// #include<stdio.h>
+// int main() {
+//     printf("1,2,3,4,5,6,7");
+//     return 0;
+
+#include <stdio.h>
+int main()
+{
+    printf("5+6 is:%d \n",5+6);
+    printf("4+3 is =%d\n",4+3);
+
+    printf("5+6 is =%d\n 4+3 is =%d %d",5+6,4+3,5+3);
     return 0;
 }
